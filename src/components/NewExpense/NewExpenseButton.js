e20ee3e244ab09801a95ef58bab927031cb7705a
Bronwyn Greeff <bronwyngreeff@BRONWYNs-MacBook-Pro.local>
2022-04-21 09:28:42 +0200

    First commit

---
+++ src/components/NewExpense/NewExpenseButton.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./NewExpenseButton.css";
+
+const NewExpenseButton = () => {
+  return (
+    <div className="new-expense">
+      <button>Add New Expense</button>
+    </div>
+  );
+};
+
+export default NewExpenseButton;
